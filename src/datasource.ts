c17eb6b1dce1f9c3ba6e5d896128614a284039d4
mosuka <minoru.osuka@gmail.com>
2022-01-19 23:41:57 +0900

    first commit

---
+++ src/datasource.ts
@@ -0,0 +1,55 @@
+import defaults from 'lodash/defaults';
+
+import {
+  DataQueryRequest,
+  DataQueryResponse,
+  DataSourceApi,
+  DataSourceInstanceSettings,
+  MutableDataFrame,
+  FieldType,
+} from '@grafana/data';
+
+import { MyQuery, MyDataSourceOptions, defaultQuery } from './types';
+
+export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+  endpoint: string | undefined;
+  constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
+    super(instanceSettings);
+    this.endpoint = instanceSettings.jsonData.endpoint;
+  }
+
+  async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
+    const { range } = options;
+    const from = range!.from.valueOf();
+    const to = range!.to.valueOf();
+
+    // Return a constant for each query.
+    const data = options.targets.map(target => {
+      const query = defaults(target, defaultQuery);
+      return new MutableDataFrame({
+        refId: query.refId,
+        fields: [
+          { name: 'Time', values: [from, to], type: FieldType.time },
+          { name: 'Value', values: [query.constant, query.constant], type: FieldType.number },
+        ],
+      });
+    });
+
+    return { data };
+  }
+
+  async testDatasource() {
+    // Implement a health check for your data source.
+    if (!this.endpoint) {
+      return {
+        status: 'failure',
+        message: 'Missing endpoint',
+      };
+    }
+
+    return {
+      status: 'success',
+      message: 'Success',
+    };
+  }
+}
